b6ab219195d39c0aa1d1cd0e505c63835752fa0d
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-08-23 14:13:09 +0530

    Add About section to the website.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Home from "./components/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
+import About from "./components/About.jsx";
+
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function About() {
+  return (
+    <div name="about" className=" bg-[#0a192f] h-screen w-full text-gray-300">
+      <div className=" flex flex-col justify-center items-center w-full h-full">
+        <div className=" max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className=" pb-8 pl-4 sm:text-right">
+            <p className=" text-4xl font-bold inline border-b-4 border-blue-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className=" max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className=" sm:text-right text-4xl font-bold">
+            Hi, I'm Ajoy, nice to meet you. Please take a look around.
+          </div>
+          <div>
+            As a Full-Stack Web Developer specializing in the MERN stack, My
+            technical skills are complemented by excellent problem-solving
+            abilities, effective communication. I am well-prepared and eager to
+            take on challenging projects or roles that require a dynamic and
+            skilled approach to web development.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
