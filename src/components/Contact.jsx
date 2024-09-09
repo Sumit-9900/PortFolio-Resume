7adbf2a071939fab22d96ac4b584db863f60f3aa
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-09-09 23:57:35 +0530

    Add the dotenv package for env file.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Contact() {
+  const ref = useRef();
+  const formDone = () => {
+    ref.current.submit();
+    ref.current.value = "";
+  };
   return (
     <div
       name="contact"
       className=" w-full min-h-screen bg-[#0a192f]  flex justify-center items-center p-4 lg:pt-16"
     >
-      <form action="" className=" max-w-[600px] w-full flex flex-col">
+      <form
+        action={`https://getform.io/f/${import.meta.env.VITE_ENDPOINT}`}
+        method="POST"
+        className=" max-w-[600px] w-full flex flex-col"
+      >
         <div className=" pb-8 text-gray-300">
           <p className=" text-4xl font-bold inline border-b-4 border-blue-600">
             Contact
@@ -20,20 +29,30 @@ function Contact() {
           type="text"
           name="name"
           placeholder="Name"
+          required={true}
+          ref={ref}
         />
         <input
           className=" bg-[#ccd6f6] my-6 p-2 rounded-md text-black outline-none"
           type="email"
           name="email"
           placeholder="Email"
+          required={true}
+          ref={ref}
         />
         <textarea
           className=" p-2 bg-[#ccd6f6] rounded-md text-black outline-none"
           name="message"
           placeholder="Message"
+          required={true}
           rows={10}
+          ref={ref}
         ></textarea>
-        <button className=" text-white border-2 rounded-md hover:bg-blue-600 hover:border-blue-600 px-4 py-3 my-8 mx-auto flex items-center justify-center duration-300">
+        <button
+          type="submit"
+          onClick={formDone}
+          className=" text-white border-2 rounded-md hover:bg-blue-600 hover:border-blue-600 px-4 py-3 my-8 mx-auto flex items-center justify-center duration-300"
+        >
           Let's collaborate
         </button>
       </form>
